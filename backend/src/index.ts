8bbbf125d1cdb0936b878e30eb46981b80caf21c
Aniket Singh <87822922+Aniket6990@users.noreply.github.com>
2022-10-14 12:42:36 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,8 @@ dotenv.config();
 import Moralis from "moralis/node";
 import { mongoConnect } from "./libs/mongo";
 
-const serverUrl = "https://u7hxzk1zphhy.usemoralis.com:2053/server";
-const appId = "bbzmlOf352Kd7AM83Icgu9e9XES7gKAwgv0sEFgm";
+const serverUrl = "";
+const appId = "";
 
 Promise.all([
   Moralis.start({ serverUrl, appId }), //
